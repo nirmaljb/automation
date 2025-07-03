1744d63381029ce398c99e1c281992b8978f1a5e
Nirmal Jyoti Biswas <nirmaljyotib@gmail.com>
2025-07-03 23:55:01 +0530

    had to commit something

--- hook/src/index.ts
+++ hook/src/index.ts
@@ -2,7 +2,6 @@ import express from "express";
 
 const app = express();
 
-//https://hooks.zapier.com/hooks/catch/23593399/ubwh5qe/
 
 app.post('/hooks/catch/:userId/:zapId', (req, res) => {
     const userId = req.params.userId;    
